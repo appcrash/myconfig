ad6b88d92315822a17b504fdbe44993620a46656
czdyh339@gmail.com <czdyh339@gmail.com@e64312b6-186b-11f7-34b3-b75fced82de8>
2011-08-03 05:26:56 +0000

    [emacs]python flymake using pyflakes
    git-svn-id: https://myconfig.googlecode.com/svn/trunk@24 e64312b6-186b-11f7-34b3-b75fced82de8

--- emacs.d/myconfig/prog_python.el
+++ emacs.d/myconfig/prog_python.el
@@ -1,3 +1,17 @@
 (add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
 
+(when (load "flymake" t) 
+         (defun flymake-pyflakes-init () 
+           (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                              'flymake-create-temp-inplace)) 
+              (local-file (file-relative-name 
+                           temp-file 
+                           (file-name-directory buffer-file-name)))) 
+             (list "pyflakes" (list local-file)))) 
+
+         (add-to-list 'flymake-allowed-file-name-masks 
+                  '("\\.py\\'" flymake-pyflakes-init)))
+
 (provide 'prog_python)
+
+
