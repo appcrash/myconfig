8ac8f8c498fca9d1bf08c86eec60886c4918b556
cosmosgleam <cosmosgleam@gmail.com>
2020-02-19 13:14:55 +0800

    [emacs] correct erlang mode enter key

--- emacs.d/myconfig/prog_erlang.el
+++ emacs.d/myconfig/prog_erlang.el
@@ -1,7 +1,7 @@
 (add-hook 'erlang-mode-hook
   (lambda ()
     (delete-trailing-whitespace-local)
-    (define-key erlang-mode-map "\C-m" 'newline-and-indent)
+    ;(define-key erlang-mode-map "\C-m" 'newline-and-indent)
     (setq erlang-indent-level 2)))
 
 (provide 'prog_erlang)
