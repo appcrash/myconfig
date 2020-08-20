fc6f9d24e611edf13291c0064791c5cd2ce351f6
cosmosgleam <cosmosgleam@gmail.com>
2020-08-20 11:27:43 +0800

    [emacs]: set mytodo file as global variable

--- emacs.d/myconfig/prog_org.el
+++ emacs.d/myconfig/prog_org.el
@@ -1,7 +1,7 @@
 (setq org-root "c:/Users/yh/Dropbox/Apps/MobileOrg/")
+(defvar mytodo_file (concat org-root "todo.org")) 
 
 (add-hook 'after-init-hook (lambda ()
-    (setq mytodo_file (concat org-root "todo.org")) 
     (setq org-agenda-files  (list mytodo_file))
     (setq org-default-notes-file (concat org-root "note.org"))
     (setq org-log-done t)))
